e3baf0ea5eed569df156efdd6213e8cdf82e3d04
ApollineSetan <setan.apolline@gmail.com>
2025-02-26 09:52:26 +0100

    initial commit

---
+++ script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS mydatabase;
+USE mydatabase;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(100)
+)Engine=InnoDB;
